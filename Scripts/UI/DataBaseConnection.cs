02d56f3df038089a77dc05b5b1fa97d82e1fc998
mminus0707 <egeeren.2003@hotmail.com>
2024-03-19 20:43:42 +0000

    Database Insertion Added
    INSERT Working

---
+++ Scripts/UI/DataBaseConnection.cs
@@ -0,0 +1,10 @@
+using MySql.Data.MySqlClient;
+
+namespace GroupProject.Scripts;
+
+class dataBaseConnection{
+    public MySqlConnection connection;
+    public void Connect(){
+        connection = new MySqlConnection("Datasource = 127.0.0.1;username=root;password=;database=let_me_show_you");
+    }
+}
