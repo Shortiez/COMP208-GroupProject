9b58db328690094a27d25f51c7c3bde139de0b36
Ben Gotts <gottsie11@gmail.com>
2024-03-30 22:18:15 +0000

    Account Details page finished

--- ViewModels/AccountPageViewModel.cs
+++ ViewModels/AccountPageViewModel.cs
@@ -1,7 +1,26 @@
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
+using GroupProject.Models;
 
 namespace GroupProject.ViewModels;
 
 public partial class AccountPageViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private UserDataModel _userData = App.MainWindowViewModel.User;
+    [ObservableProperty]
+    private string? _username = string.Empty;
+    [ObservableProperty]
+    private string? _email = string.Empty;
+    [ObservableProperty]
+    private string? _password = string.Empty;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        
+        Username = UserData.Username;
+        Email = UserData.Email;
+        Password = UserData.Password;
+    }
 }
