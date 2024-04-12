f8cd82a2cc6a0d6691d3563aa3b68fc7eb9e1e7b
dustysaturn <151056468+dustysaturn@users.noreply.github.com>
2024-04-12 22:27:30 +0100

    Binary Addition progress

--- ViewModels/BinaryAdditionViewModels/BinaryAdditionLearnPageViewModel.cs
+++ ViewModels/BinaryAdditionViewModels/BinaryAdditionLearnPageViewModel.cs
@@ -15,7 +15,75 @@ namespace GroupProject.ViewModels;
 public partial class BinaryAdditionLearnPageViewModel : ViewModelBase
 {
     // generate a question
-    public Bitmap? ImageFromBinding { get;} = ImageHelper.LoadFromResource("Assets/button-return.png");
+    [ObservableProperty]
+    private TopicContentModel topicContentModel;
+    
+    private BinaryAdditionQuizGenerator _quizGenerator = new BinaryAdditionQuizGenerator();
+    private QuizQuestion<int> _currentQuestion;
+
+    [ObservableProperty]
+    private int[] _num1digits;
+    [ObservableProperty]
+    private int[] _num2digits;
+    [ObservableProperty]
+    private int[] _answerdigits;
+    [ObservableProperty]
+    private int _remainder;
+    [ObservableProperty]
+    private int _index;
+
+    
+    public BinaryAdditionLearnPageViewModel()
+    {
+
+    }
+
+    public BinaryAdditionLearnPageViewModel(QuizQuestion<int> quizQuestion)
+    {
+        _currentQuestion = quizQuestion;
+    }
+
+    private void populateNumbers()
+    {
+        // loop through option 1, populating ints
+
+        // loop through option 2, populating ints
+
+        // loop through answer, populating ints
+    }
+
+    private void compareNumbers()
+    {
+        if(_index > 7)
+        {
+
+        }
+        else{
+            if(_num1digits[_index] == null || _num2digits[_index] == null)
+            {
+                // calculate complete. 
+            }
+            else{
+                // compare current index nums
+                    // case 0 + 1
+                        // answer[i] = 1
+                    // case 1 + 1
+                        // answer[i] = 0
+                        // remainder = 1
+                    // case 1 + 0
+                        // answer[i] = 1
+                    // case 1 + 1 + 1
+                    // case 0 + 0
+            }
+        }
+        // if one of them is null, calculation is complete. 
+    }
+
+    private void generateQuestion()
+    {
+
+    }
+
 
     [RelayCommand]
     private void OnClickBackToHome()
--- ViewModels/BinaryAdditionViewModels/BinaryAdditionQuizPageViewModel.cs
+++ ViewModels/BinaryAdditionViewModels/BinaryAdditionQuizPageViewModel.cs
@@ -2,8 +2,6 @@ using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Avalonia.Controls;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
 using GroupProject.Scripts.Questions;
 using GroupProject.Scripts.Questions.Quizzes.BinaryAddition;
 using GroupProject.Models;
@@ -14,7 +12,6 @@ namespace GroupProject.ViewModels;
 
 public partial class BinaryAdditionQuizPageViewModel : ViewModelBase
 {
-    public Bitmap? ImageFromBinding { get;} = ImageHelper.LoadFromResource("Assets/button-return.png");
 
     [ObservableProperty]
     private TopicContentModel topicContentModel;
@@ -23,47 +20,96 @@ public partial class BinaryAdditionQuizPageViewModel : ViewModelBase
 
     private BinaryAdditionQuizGenerator _quizGenerator = new BinaryAdditionQuizGenerator();
     private int _selectedOption;
-    public int SelectedOption {
-        get => _selectedOption; 
-        set {_selectedOption = value; OnPropertyChanged();}}
     private QuizQuestion<int> _currentQuestion;
 
+    public BinaryAdditionQuizPageViewModel()
+    {
+        GenerateNewQuestion();
+    }
+
     [ObservableProperty]
-    private string _questionTitleBlock = "What is 1010 + 0101?";
+    private string _questionTitleBlock;
     [ObservableProperty]
     private ObservableCollection<int> _questionOptions = new ObservableCollection<int>();
     [ObservableProperty]
-    public string _answerBlock = "";
+    public string _answerBlock;
+    
+    [ObservableProperty]
+    private int _optionOne;
+    [ObservableProperty]
+    private int _optionTwo;
+    [ObservableProperty]
+    private int _optionThree;
+    [ObservableProperty]
+    private int _optionFour;
+    [ObservableProperty]
+    private int _optionFive;
+    
+    [RelayCommand]
+    private void OneClicked()
+    {
+        _selectedOption = OptionOne;
+    }
+
+    [RelayCommand]
+    private void TwoClicked()
+    {
+        _selectedOption = OptionTwo;
+    }
+
+    [RelayCommand]
+    private void ThreeClicked()
+    {
+        _selectedOption = OptionThree;
+    }
+
+    [RelayCommand]
+    private void FourClicked()
+    {
+        _selectedOption = OptionFour;
+    }
+
+    [RelayCommand]
+    private void FiveClicked()
+    {
+        _selectedOption = OptionFive;
+    }
 
     [RelayCommand]
     private void GenerateNewQuestion()
     {
         _currentQuestion = _quizGenerator.NewQuestion();
-        
         QuestionTitleBlock = _currentQuestion.QuestionTitle;
         QuestionOptions = new ObservableCollection<int>(_currentQuestion.Options);
+
+        OptionOne = QuestionOptions[0];
+        OptionTwo = QuestionOptions[1];
+        OptionThree = QuestionOptions[2];
+        OptionFour = QuestionOptions[3];
+        OptionFive = QuestionOptions[4];
+
+        AnswerBlock = "";
     }
     
     [RelayCommand]
-    private void SubmitAnswer(){
-        if(_selectedOption == null){
+    private void SubmitAnswer()
+    {
+        if(_selectedOption == null)
+        {
             return;
         }
 
-        var selectedOptionInt = _selectedOption;
-        
-        if(selectedOptionInt == _currentQuestion.Answer)
+
+        if(_selectedOption == _currentQuestion.Answer)
         {
-            // Correct
-            _answerBlock = "Correct!";
-            // _userStatistics.UpdateExistingRecord(1,0);
+            AnswerBlock = "Correct!";
+            //_userStatistics.UpdateExistingRecord(1,0);
         }
         else
         {
-            // Incorrect
-            _answerBlock = "Incorrect!" + "\n"
+            AnswerBlock = "Incorrect!" + "\n"
                         + "The correct answer was " + _currentQuestion.Answer;
-            // _userStatistics.UpdateExistingRecord(0,1);
+            //_userStatistics.UpdateExistingRecord(0,1);
         }
     }
 
@@ -79,5 +125,14 @@ public partial class BinaryAdditionQuizPageViewModel : ViewModelBase
         };
         
         App.MainWindowViewModel.CurrentContent = topic;
+    }
+
+    [RelayCommand]
+    private void OnClickToLearn()
+    {
+        var topicName = "Binary Addition";
+        Console.WriteLine(topicName);
+
+        App.MainWindowViewModel.CurrentContent = new BinaryAdditionLearnPageViewModel(_currentQuestion);
     }
 }
